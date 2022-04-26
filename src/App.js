c758126d55be327dd7031a838778230df7d84462
hannattaylor <hannataylor1993@gmail.com>
2022-04-26 17:49:12 +0200

    #4 data to JSON-file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import scrumJson from "./scrum.json";
 
 function App() {
+  let scrum = scrumJson;
+
+  //Funktion för att returnera JSON property som innerHTML i valda element- för att kunna klicka sig fram på sidan //
+  function createTag(scrumEl) {
+    return { __html: scrumEl };
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* test av innerHtml */}
+      <h3>Vad är Scrum?</h3>
+      <p id="about" dangerouslySetInnerHTML={createTag(scrum.about)}></p>
+
+      <p id="history" dangerouslySetInnerHTML={createTag(scrum.history)}></p>
+      <h3>Roller</h3>
+      <p
+        id="productOwner"
+        dangerouslySetInnerHTML={createTag(scrum.roles.productOwner)}
+      ></p>
+      <p
+        id="scrumMaster"
+        dangerouslySetInnerHTML={createTag(scrum.roles.scrumMaster)}
+      ></p>
+      <p
+        id="scrumTeam"
+        dangerouslySetInnerHTML={createTag(scrum.roles.scrumTeam)}
+      ></p>
+      <h3>Aktiviteter</h3>
+      <p id="sprint" dangerouslySetInnerHTML={createTag(scrum.sprint)}></p>
+      <p
+        id="sprintPlanning"
+        dangerouslySetInnerHTML={createTag(scrum.sprintPlanning)}
+      ></p>
+      <p
+        id="dailyScrum"
+        dangerouslySetInnerHTML={createTag(scrum.dailyScrum)}
+      ></p>
+      <p
+        id="sprintReview"
+        dangerouslySetInnerHTML={createTag(scrum.sprintReview)}
+      ></p>
+      <p
+        id="sprintRetrospective"
+        dangerouslySetInnerHTML={createTag(scrum.sprintRetrospective)}
+      ></p>
+      <h3>Scrum Artefakter</h3>
+      <p
+        id="aboutArtefacts"
+        dangerouslySetInnerHTML={createTag(scrum.artefacts.about)}
+      ></p>
+      <p
+        id="backlog"
+        dangerouslySetInnerHTML={createTag(scrum.artefacts.backlog)}
+      ></p>
+      <p
+        id="sprintBacklog"
+        dangerouslySetInnerHTML={createTag(scrum.artefacts.sprintBacklog)}
+      ></p>
+      <p
+        id="inkrement"
+        dangerouslySetInnerHTML={createTag(scrum.artefacts.inkrement)}
+      ></p>
     </div>
   );
 }
